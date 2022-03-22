4804f31ef825d290c097a31aa45b632ebe6f35ce
Yuejiang Liu <yuejiang.liu@outlook.com>
2022-03-22 14:53:04 +0100

    Update client.py

--- python/client.py
+++ python/client.py
@@ -1,8 +1,6 @@
-# MICRO-453 TP-13
-# Visual Navigation: A Deep Learning Perspective
-# VITA, EPFL 
+# VITA, EPFL
 
-import cv2
+#import cv2
 import socket
 import sys
 import numpy
@@ -11,15 +9,34 @@ import binascii
 
 from PIL import Image
 from detector import Detector
+import argparse
 
-host = '127.0.0.1'  # The server's hostname or IP address
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
 
+parser.add_argument('-c', '--checkpoint',
+                    help=('directory to load checkpoint'))
+parser.add_argument('-i','--ip-address',
+                    help='IP Address of robot')
+parser.add_argument('--instance-threshold', default=0.0, type=float,
+                    help='Defines the threshold of the detection score')
+parser.add_argument('-d', '--downscale', default=8, type=int,
+                    help=('downscale of the received image'))
+parser.add_argument('--square-edge', default=401, type=int,
+                    help='square edge of input images')
+
+args = parser.parse_args()
+
+##### IP Address of server #########
+host = args.ip_address #'128.179.150.43'  # The server's hostname or IP address
 ####################################
 port = 8081        # The port used by the server
 
 # image data
-width = 80
-height = 60 
+downscale = args.downscale
+width = int(640/downscale)
+height = int(480/downscale)
 channels = 3
 sz_image = width*height*channels
 
@@ -31,7 +48,7 @@ except socket.error:
     print('Failed to create socket')
     sys.exit()
 
-print('# Getting remote IP address') 
+print('# Getting remote IP address')
 try:
     remote_ip = socket.gethostbyname( host )
 except socket.gaierror:
@@ -43,10 +60,10 @@ print('# Connecting to server, ' + host + ' (' + remote_ip + ')')
 s.connect((remote_ip , port))
 
 # Set up detector
-detector = Detector()
-detector.load('./saved_model.pth')
+arguments = ["--checkpoint",args.checkpoint,"--pif-fixed-scale", "1.0", "--instance-threshold",args.instance_threshold]
+detector = Detector(arguments,input_size = args.square_edge)
 
-#Image Receiver 
+#Image Receiver
 net_recvd_length = 0
 recvd_image = b''
 
@@ -64,36 +81,29 @@ while True:
     if net_recvd_length == sz_image:
 
         pil_image = Image.frombytes('RGB', (width, height), recvd_image)
-        opencvImage = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
-        opencvImage = cv2.cvtColor(opencvImage,cv2.COLOR_BGR2RGB)
+        # opencvImage = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
+        # opencvImage = cv2.cvtColor(opencvImage,cv2.COLOR_BGR2RGB)
+        #
+        # cv2.imshow('Test window',opencvImage)
 
-        cv2.imshow('Test window',opencvImage)
-        cv2.waitKey(1)
-
+        #cv2.waitKey(1)
         net_recvd_length = 0
         recvd_image = b''
 
         #######################
         # Detect
         #######################
-        bbox, bbox_label = detector.forward(opencvImage)
-        
+        bbox, bbox_label = detector.forward(pil_image)
+
         if bbox_label:
-            print(bbox)
+            print("BBOX: {}".format(bbox))
+            print("BBOX_label: {}".format(bbox_label))
         else:
             print("False")
 
         # https://pymotw.com/3/socket/binary.html
-        values = (bbox[0], bbox[1], bbox[2], bbox[3], float(bbox_label[0]))
-        # values = (50.0, 30.0, 10.0, 10.0, 1.0)
+        values = (bbox[0], bbox[1], 10, 10, float(bbox_label[0]))
 
-        # #Test Controller
-        # cnt = cnt + 1
-        # if cnt > 50:
-        #     direction = - direction
-        #     cnt = 0
-        # values = (40.0 + direction * 20.0, 30.0, 10.0, 20.0, 1.0)
-        
         packer = struct.Struct('f f f f f')
         packed_data = packer.pack(*values)
 
